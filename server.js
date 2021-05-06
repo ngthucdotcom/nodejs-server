c3ef42440cb5f2ba42881af70fa60b9048039bc0
ngthuc <lenguyenthuc.edu@gmail.com>
2021-05-06 16:16:18 +0700

    Init template

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var moment = require("moment");
+var router = express.Router();
+
+app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+router.get("/", (req, res) => {
+    const timer = moment().format("YYYY-MM-DD HH:mm:ss");
+    console.log(timer);
+    res.json({
+        "received": timer
+    });
+});
+
+router.post("/api/logger", (req, res) => {
+    // console.log(moment().format("YYYY-MM-DD HH:mm:ss"));
+    console.log(req.body);
+    // console.log(req.body.message);
+    // res.json(req.body);
+    res.json({
+        "received": moment().format("YYYY-MM-DD HH:mm:ss")
+    });
+});
+
+app.use(router);
+
+var server = app.listen(11111, function() {
+    console.log('Server listening on port ' + server.address().port);
+});
+
+module.exports = app;
