0c54d1475b38b8eb937eb679b68005b6b3e2ea2c
thucln <thucln@vng.com.vn>
2022-06-19 17:49:00 +0700

    Update code

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require('cors');
+var moment = require("moment");
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    const timer = moment().format("YYYY-MM-DD HH:mm:ss");
+    console.log({
+        "received": timer,
+        "agent": req.get('User-Agent')
+    });
+    res.json({
+        "received": timer
+    });
+});
+
+router.post("/", (req, res) => {
+    const timer = moment().format("YYYY-MM-DD HH:mm:ss");
+    console.log({
+        "received": timer,
+        "agent": req.get('User-Agent'),
+        "body": req.body
+    });
+    res.json({
+        "received": timer
+    });
+});
+
+app.use(express.json());
+app.use(router);
+app.use(cors());
+
+// Export the router
+module.exports = app;
